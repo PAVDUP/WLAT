ee50d2c8a69de4b33352cf1c20098cd3172957cb
kbojk <164342688+kbojk@users.noreply.github.com>
2024-08-07 18:00:16 +0900

    기본적인 기능 완료

--- WLAT/Assets/_Project/Script/RandomImages.cs
+++ WLAT/Assets/_Project/Script/RandomImages.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class RandomImages : MonoBehaviour
 {
     public Sprite[] sprites;
+    public TextMeshProUGUI text;
+
     Image thisImage;
 
     void Start()
@@ -14,5 +17,21 @@ public class RandomImages : MonoBehaviour
 
         int ranNum = Random.Range(0, sprites.Length);
         thisImage.sprite = sprites[ranNum];
+
+        switch (ranNum)
+        {
+            case 0:
+                text.text = "저장을 생활화하자!";
+                break;
+            case 1:
+                text.text = "잠잘 때도 일을 하는 건... 싫어...";
+                break;
+            case 2:
+                text.text = "미라클 모닝! 햇살도 날 이길 수 없지!";
+                break;
+            case 3:
+                text.text = "카페인과 함께라면 두려울게 없지!...";
+                break;
+        }
     }
 }
